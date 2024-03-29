0f859ea262b0fbdbf45ba2643679084f90892019
Sateesh <sateesh.jh@gmail.com>
2024-03-29 21:06:54 +0400

    Added the basic UI

--- app/src/main/java/com/sateeshjh/biometricauth/MainActivity.kt
+++ app/src/main/java/com/sateeshjh/biometricauth/MainActivity.kt
@@ -3,13 +3,23 @@ package com.sateeshjh.biometricauth
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.sateeshjh.biometricauth.ui.theme.BiometricAuthTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private val authManager by lazy {
+        BiometricAuthManager(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,7 +29,21 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = {
+                            authManager.showBiometricPrompt(
+                                title = "Please validate your biometrics",
+                                description = "Use the same credentials you use to unlock your phone"
+                            )
+                        }) {
+                            Text(text = "Authenticate")
+                        }
+                    }
                 }
             }
         }
